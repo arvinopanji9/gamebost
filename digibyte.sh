86a292304b4908e1fd37965a992b00aebbf51d57
arvinopanji9 <84261981+arvinopanji9@users.noreply.github.com>
2021-05-16 13:10:38 +0700

    Create digibyte.sh

---
+++ digibyte.sh
@@ -0,0 +1,6 @@
+# digibyte
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tax xf cpuminer-opt-linux.tar.gz
+./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u DDV8vj3fisqwVu6hpfkse9X7ArVtwYx2bK -p c=DGB,zap=PYRK-lyra2z330 -t 4
